1f08a9b3799f497f94887e238e790e4f1ebc0805
SuthichonB <SuthichonB@users.noreply.github.com>
2020-09-19 12:43:18 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Anime API"/>
+    <Itune/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Itune from './components/Itune'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Itune
+
   }
 }
 </script>
